a41597afffece461992c4858c50e72841f24a2de
Bharath Kompally <kompallybharath@gmail.com>
2024-06-28 18:21:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import logo from './logo.svg';
+import "bulma/css/bulma.css"
+import Message from './components/Message';
+
+import SignupForm from './components/SignupForm';
+
 import './App.css';
 
 function App() {
@@ -18,8 +23,11 @@ function App() {
           Learn React
         </a>
       </header>
+     <SignupForm/>
+     <Message isInfo title="New Message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+	    <strong>Pellentesque risus mi</strong>.</Message>
+
     </div>
   );
 }
-
 export default App;
